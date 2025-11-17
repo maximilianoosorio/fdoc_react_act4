68ab69bdf1c55f47b732a1375f4e05c3a0ada20e
maximilianoosorio <maxinal012208@gmail.com>
2025-11-17 09:31:06 -0500

    Agrega nuevos estilos a el EventList

--- app/components/EventList.jsx
+++ app/components/EventList.jsx
@@ -12,7 +12,7 @@ const events = [
 function EventList() {
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6 text-center">Proximos Eventos</h1>
+      <h1 className="text-3xl text-black font-bold mb-6 text-center">Proximos Eventos</h1>
 
       <>
         {events.map((event) => (
